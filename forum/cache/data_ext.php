27dc744421a25b888bd3d95d0d84d08eef2dbc52
Maxime Chenot <maxime.c25@gmail.com>
2015-11-17 20:56:14 +0100

    Forum devient site principal

--- forum/cache/data_ext.php
+++
@@ -1,4 +0,0 @@
-<?php exit; ?>
-1479239014
-314
-a:2:{s:13:"gramziu/anami";a:4:{s:8:"ext_name";s:13:"gramziu/anami";s:10:"ext_active";s:1:"1";s:9:"ext_state";s:4:"b:0;";s:8:"ext_path";s:18:"ext/gramziu/anami/";}s:11:"phpbb/pages";a:4:{s:8:"ext_name";s:11:"phpbb/pages";s:10:"ext_active";s:1:"1";s:9:"ext_state";s:4:"b:0;";s:8:"ext_path";s:16:"ext/phpbb/pages/";}}
--- forum/cache/data_ext_finder.php
+++
@@ -1,4 +0,0 @@
-<?php exit; ?>
-1479239014
-278
-a:2:{s:32:"96e42a29c83e42edfd0d659aa01a761e";a:1:{i:0;a:4:{s:10:"named_path";s:34:"ext/phpbb/pages/config/routing.yml";s:8:"ext_name";s:11:"phpbb/pages";s:4:"path";s:23:"ext/phpbb/pages/config/";s:8:"filename";s:11:"routing.yml";}}s:32:"e1ce93a4d0659dd0534b3fc70c33cc35";a:0:{}}
